a27ce66e20cac869e2ca40d423ed2f7766295ebb
Guilherme Marques <71295632+GuilhermeMPCosta@users.noreply.github.com>
2023-03-08 10:28:28 -0300

    adição do onchange para o qrcode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import './App.css';
+import { useState } from 'react';
 import QRCode from 'qrcode.react';
+// import QRCodeLink from 'qrcode.react'
 
 function App() {
+  const [link, setlink]=useState('')
+  // const [qrcodelink,setqrcodelink] = useState('')
+  
+// function handleGenerate(link_url){
+//   QRCodeLink.toDataUrl(link_url,{
+//     width:600,
+//     margin:3,
+//   }, function(err, url){
+//     setqrcodelink(url);
+    
+//   })
+// }
+
+  function handleQRcode(event){
+    setlink(event.target.value);
+    // handleGenerate(event.target.value)
+  }
   return (
     <div className='container'>
 
       <QRCode
-        value='https://www.linkedin.com/in/guilhermemarquespaulino/'
+        value={link}
       />
 
-        <input className='input' placeholder='insira um link'/>
+        <input className='input' placeholder='insira um link' value={link} onChange={(e)=>handleQRcode(e)}/>
+
     </div>
   );
 }
